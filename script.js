a886ead4a45c2179ef2b7ce6466d5b6bce2cfe0e
Luiz Felipe Volpe Pinheiro Brandão <1590051@sga.pucminas.br>
2025-03-20 15:18:09 -0300

    Update 04

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const toggleButton = document.getElementById("toggle-dark-mode");
-const body = document.body;
-
-// Carregar preferência do usuário
-if (localStorage.getItem("darkMode") === "enabled") {
-    body.classList.add("dark-mode");
-}
-
-// Alternar modo
-toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-
-    // Salvar a escolha do usuário
-    if (body.classList.contains("dark-mode")) {
-        localStorage.setItem("darkMode", "enabled");
-    } else {
-        localStorage.setItem("darkMode", "disabled");
-    }
-});
-
 voltarButton.addEventListener("click", () => {
     window.location.href = "index.html";
 })
